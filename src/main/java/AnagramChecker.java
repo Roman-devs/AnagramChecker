3bf4a4c3ae6a88001a0e82d3b22faee2f4fe23f6
Roman Kuite <roman.kuite@gmail.com>
2021-04-21 13:44:06 +0200

    final product

--- src/main/java/AnagramChecker.java
+++ src/main/java/AnagramChecker.java
@@ -1,48 +1,36 @@
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
 import java.io.*;
 import java.util.*;
 
 
 public class AnagramChecker {
 
-    static int size;
-    static int count;
-    static char[] charArray;
-    static char[] words;
-
-
     public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(new File("/Users/roman/Documents/CODE/Coding Challenge No.1/src/main/resources/words_dictionary.json.txt"));
+        Scanner scanner = new Scanner(new File("src/main/resources/words_dictionary.json.txt"));
         ArrayList<String> words = new ArrayList<>();
         while (scanner.hasNext()) {
             words.add(scanner.next());
         }
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Printing Anagram from given File for abcfinlab challenge!\n");
-        Map<String, List<String>> Anagrams = findAnagrams(words);
+        Map<String, List<String>> setOfAnagrams = findAnagrams(words);
 
-        for (Map.Entry<String, List<String>> entry : Anagrams.entrySet()) {
+        for (Map.Entry<String, List<String>> entry : setOfAnagrams.entrySet()) {
             {
                 List<String> values = entry.getValue();
-                String a = values.toString();
-                a = a.substring(1, a.length() - 1);
+                String anagramsToBePrinted = values.toString();
                 if (values.size() >= 2) {
-                    System.out.println(">>> " + a);
+                    System.out.println(">>> " + anagramsToBePrinted);
                 }
             }
         }
     }
 
     private static Map<String, List<String>> findAnagrams(List<String> words) {
-        Map<String, List<String>> anagrams;
+        Map<String, List<String>> anagrams = new HashMap<>();
         if (words == null || words.isEmpty()) {
             return null;
         }
-        anagrams = new HashMap<>();
         for (String word : words) {
             if (word.length() > 1) {
                 char[] charArray = word.trim().toCharArray();
